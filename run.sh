4a166aa1ede7e12656e09fa66d9717583f3bfd03
Martin Popovski <martinkozle@yahoo.com>
2024-12-01 15:13:44 +0100

    Update run.py

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ DAY=$(printf "%02d" $1)
 PART=$2
 
 if [[ -n "$3" && "$3" == "in" ]]; then
-    python -m src.days.day_${DAY}.part$2 <&0
+    python -m src.days.day_${DAY}.part${PART} <&0
 else
-    python -m src.days.day_${DAY}.part$2 < inputs/day_${DAY}.txt
+    python -m src.days.day_${DAY}.part${PART} < inputs/day_${DAY}.txt
 fi
