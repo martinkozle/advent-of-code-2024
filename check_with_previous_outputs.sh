0e17528762dce85e33e629377ca6a16959878155
Martin Popovski <martinkozle@yahoo.com>
2024-12-02 07:06:48 +0100

    Add outputs saving and checking

---
+++ check_with_previous_outputs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# Useful if changing common code that may affect previous days
+
+for i in $(seq 1 25); do
+    DAY=$(printf "%02d" $i)
+    INPUT=inputs/day_${DAY}.txt
+    if [ ! -f $INPUT ]; then
+        echo "# DAY=${DAY} No input"
+        continue
+    fi
+
+    for PART in $(seq 1 2); do
+
+        INPUT=inputs/day_${DAY}.txt
+
+        OUTPUT=$(python -m src.days.day_${DAY}.part${PART} < $INPUT 2>/dev/null)
+
+        OUTPUT_FILE="outputs/day_${DAY}.part${PART}.txt"
+
+        # Compare output with previous output
+        if [ -f "$OUTPUT_FILE" ]; then
+            PREVIOUS_OUTPUT=$(cat $OUTPUT_FILE)
+            if [ "$OUTPUT" != "$PREVIOUS_OUTPUT" ]; then
+                echo "# DAY=${DAY} PART=${PART} Output differs from previous output"
+                echo "# Previous output:"
+                echo $PREVIOUS_OUTPUT
+                echo "# Current output:"
+                echo $OUTPUT
+            else
+                echo "# DAY=${DAY} PART=${PART} OK"
+            fi
+        else
+            echo "# DAY=${DAY} PART=${PART} No previous output"
+        fi
+    done
+done
--- run.sh
+++ run.sh
@@ -3,8 +3,32 @@
 DAY=$(printf "%02d" $1)
 PART=$2
 
+# Check if input should be stdin
 if [[ -n "$3" && "$3" == "in" ]]; then
-    python -m src.days.day_${DAY}.part${PART} <&0
+    INPUT=/dev/stdin
 else
-    python -m src.days.day_${DAY}.part${PART} < inputs/day_${DAY}.txt
+    INPUT=inputs/day_${DAY}.txt
 fi
+
+OUTPUT=$(python -m src.days.day_${DAY}.part${PART} < $INPUT)
+
+echo "# OUTPUT:"
+echo $OUTPUT
+
+if [ ! -d "outputs" ]; then
+    mkdir "outputs"
+fi
+
+OUTPUT_FILE="outputs/day_${DAY}.part${PART}.txt"
+
+# Compare output with previous output
+if [ "$INPUT" != "/dev/stdin" ] && [ -f "$OUTPUT_FILE" ]; then
+    PREVIOUS_OUTPUT=$(cat $OUTPUT_FILE)
+    if [ "$OUTPUT" != "$PREVIOUS_OUTPUT" ]; then
+        echo "# Output differs from previous output:"
+        echo $PREVIOUS_OUTPUT
+    fi
+fi
+
+
+echo $OUTPUT > $OUTPUT_FILE
