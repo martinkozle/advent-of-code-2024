cfeda30aad9b208240fd3cfb0e47599cce0d934b
Martin Popovski <martinkozle@yahoo.com>
2024-12-05 06:26:29 +0100

    Add day 5 solutions

---
+++ src/days/day_05/part1.py
@@ -0,0 +1,47 @@
+from src.prep import run_with_prep
+from src.timing import timing
+
+
+@timing
+def main(inp: str) -> None:
+    rules_str, updates_str = inp.split("\n\n")
+    rules = [
+        tuple(int(rule) for rule in rule_str.split("|"))
+        for rule_str in rules_str.split("\n")
+    ]
+    updates = [
+        list(map(int, update_str.split(","))) for update_str in updates_str.split("\n")
+    ]
+
+    rules_dict: dict[int, set[int]] = {}
+    for left, right in rules:
+        if left not in rules_dict:
+            rules_dict[left] = set()
+        rules_dict[left].add(right)
+
+    total = 0
+
+    for update in updates:
+        correct = True
+        lefts: set[int] = set()
+        rights: set[int] = set(update)
+
+        for number in update:
+            lefts.add(number)
+            rights.remove(number)
+            for right_rule in rules_dict[number]:
+                if right_rule in lefts:
+                    correct = False
+                    break
+
+            if not correct:
+                break
+
+        if correct:
+            total += update[len(update) // 2]
+
+    print(total)
+
+
+if __name__ == "__main__":
+    run_with_prep(main)
