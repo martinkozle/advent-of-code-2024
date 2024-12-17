dea159bf9742c5ebb953f45d05ca77116b0f2d05
Martin Popovski <martinkozle@yahoo.com>
2024-12-17 07:51:36 +0100

    Add daf 17 solutions

---
+++ src/days/day_17/part1.py
@@ -0,0 +1,62 @@
+import re
+
+from src.prep import run_with_prep
+from src.timing import timing
+
+
+@timing
+def main(inp: str) -> None:
+    registers: dict[str, int] = {}
+    for register in ("A", "B", "C"):
+        match = re.search(rf"Register {register}: (-?\d+)", inp)
+        if match is None:
+            raise RuntimeError("Unexpected state")
+        registers[register] = int(match.group(1))
+    match = re.search(r"Program: ((\d+,)+\d+)", inp)
+    if match is None:
+        raise RuntimeError("Unexpected state")
+    program = list(map(int, match.group(1).split(",")))
+    pointer = 0
+    outputs: list[int] = []
+    while True:
+        if pointer >= len(program):
+            break
+        opcode = program[pointer]
+        operand = program[pointer + 1]
+        operand_value: int
+        match operand:
+            case 0 | 1 | 2 | 3:
+                operand_value = operand
+            case 4:
+                operand_value = registers["A"]
+            case 5:
+                operand_value = registers["B"]
+            case 6:
+                operand_value = registers["C"]
+            case _:
+                operand_value = operand
+        match opcode:
+            case 0:
+                registers["A"] = registers["A"] // 2**operand_value
+            case 1:
+                registers["B"] = registers["B"] ^ operand
+            case 2:
+                registers["B"] = operand_value % 8
+            case 3:
+                if registers["A"] != 0:
+                    pointer = operand
+                    continue
+            case 4:
+                registers["B"] = registers["B"] ^ registers["C"]
+            case 5:
+                outputs.append(operand_value % 8)
+            case 6:
+                registers["B"] = registers["A"] // 2**operand_value
+            case 7:
+                registers["C"] = registers["A"] // 2**operand_value
+        pointer += 2
+    print(",".join(map(str, outputs)))
+
+
+if __name__ == "__main__":
+    run_with_prep(main)
